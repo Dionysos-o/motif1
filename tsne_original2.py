97dfba6e025f9e15bb4241898376d2d0ceded5d1
chengbo <chengbo.fi@aalto.fi>
2022-10-28 21:19:03 +0300

    umap ready now

--- tsne_original2.py
+++ tsne_original2.py
@@ -4,18 +4,22 @@ from sklearn.manifold import MDS
 import sim_seq2 as sim
 from matplotlib import pyplot as plt
 import random
-from scipy.optimize import curve_fit
+import scipy
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.manifold import SpectralEmbedding
 
 
 def cal_pairwise_dist(x: np.ndarray) -> np.ndarray:
     """
-    calculate pairwise distance for data by using (a-b)^2 = a^w + b^2 - 2*a*b
+    calculate pairwise distance for data by using (a-b)^2 = a^2 + b^2 - 2*a*b
     :param x: data matrix
     :param label: label of data
     :return: pairwise distance matrix of data x
     """
     sum_x = np.sum(np.square(x), 1)
     dist = np.add(np.add(-2 * np.dot(x, x.T), sum_x).T, sum_x)
+
+    dist = np.abs(dist)  # float calculation may causes negative values
     return dist
 
 
@@ -30,8 +34,8 @@ def hd_distance_umap(hd_data: np.ndarray, beta: float) -> np.ndarray:
     hd_dist = np.sqrt(cal_pairwise_dist(hd_data))
 
     for i in range(len(hd_data)):
-        hd_dist[i, :] = np.maximum(hd_dist[i, :] - np.min(hd_dist[i, hd_dist[i, :]]), 0)
-        hd_dist[i, :] = np.exp(-hd_dist / beta)
+        hd_dist[i, :] = np.maximum(hd_dist[i, :] - np.min(hd_dist[i, hd_dist[i, :].nonzero()[0]]), 0)
+        hd_dist[i, :] = np.exp(-hd_dist[i, :] / beta)
 
     return hd_dist
 
@@ -44,45 +48,115 @@ def func(x, a, b):
     :param b: parameter2
     :return: the prediction values
     """
-    return 1 / (1 + a * np.power((x, 2 * b)))
+    return 1 / (1 + a * np.power(x, 2*b))
 
 
-def fitting_curves(ld_dist_euclidean: np.ndarray, ld_dist: np.ndarray) -> (float, float):
-    """
-
-    :param ld_dist_euclidean: n^2 x 1 matrix
-    :param ld_dist: n^2 x 1 matrix
-    :return:a,b fot low dimensional distribution
-    """
-    y = func(ld_dist_euclidean, 1., 1.)
-    para = curve_fit(func, ld_dist_euclidean, ld_dist)
-    return para[0][0], para[0][0]
-
-
-def ld_psai_function(ld_data: np.ndarray, min_dist: float) -> (np.ndarray, np.ndarray):
+def ld_psai_function(ld_data: np.ndarray, min_dist: float) -> list:
     """
     :param ld_data: n x 2 matrix low dimensional data
     :param min_dist: hyperparameter to control the tightness of cluster
     :return:ld_dist_euclidean, ld_dist , n^2 x 1 matrix after psai function
     """
-    ld_dist_euclidean = np.reshape(np.sqrt(cal_pairwise_dist(ld_data)))
-    ld_dist = ld_dist_euclidean
-    for i in range(len(ld_dist_euclidean)):
-        if ld_dist_euclidean[i] <= min_dist:
-            ld_dist[i] = 1
+    ld_dist = []
+    for i in range(len(ld_data)):
+        if ld_data[i] <= min_dist:
+            ld_dist.append(1)
         else:
-            ld_dist[i] = np.exp(-ld_dist_euclidean[i] - min_dist)
-    return ld_dist_euclidean, ld_dist
+            ld_dist.append(np.exp(-ld_data[i] - min_dist))
+    return ld_dist
 
 
-def tsne(x: np.ndarray, min_dist: float, beta: float, a: float, b: float, k_neigh: int, alpha: float,
+def cal_hd_prob_row(dist_row: np.ndarray, rho_i:float, sigma: float) -> np.ndarray:
+    """
+    For each row of Euclidean distance matrix (dist_row) compute
+    probability in high dimensions (1D array)
+    :param dist_row:
+    :param rho_i:
+    :param sigma:
+    :return:
+    """
+    d = dist_row - rho_i
+    d[d < 0] = 0
+    return np.exp(- d / sigma)
+
+
+def sigma_binary_search(row: np.ndarray, rho_i: float, k_neighbors: int) -> float:
+    """
+    Solve equation k_of_sigma(sigma) = fixed_k
+    with respect to sigma by the binary search algorithm
+    :param row:
+    :param rho_i:
+    :param k_neighbors:
+    :return:
+    """
+    sigma_lower_limit = 0
+    sigma_upper_limit = 1000
+    approx_sigma = (sigma_lower_limit + sigma_upper_limit) / 2
+    for i in range(20):
+        approx_sigma = (sigma_lower_limit + sigma_upper_limit) / 2
+        entropy = np.power(2, np.sum(cal_hd_prob_row(row, rho_i, approx_sigma)))
+        if entropy < k_neighbors:
+            sigma_lower_limit = approx_sigma
+        else:
+            sigma_upper_limit = approx_sigma
+        if np.abs(k_neighbors - entropy) <= 1e-5:
+            break
+    return approx_sigma
+
+
+def cal_hd_prob(hd_data: np.ndarray, k_neighbors: int) -> np.ndarray:
+    """
+    :param hd_data:
+    :param k_neighbors:
+    :return:
+    """
+    n = len(hd_data)
+    dist = np.square(euclidean_distances(hd_data, hd_data))
+    rho = [sorted(dist[i])[1] for i in range(dist.shape[0])]
+    hd_prob = np.zeros((n, n))
+    for row_index in range(n):
+        sigma = sigma_binary_search(dist[row_index], rho[row_index], k_neighbors)
+        hd_prob[row_index] = cal_hd_prob_row(dist[row_index], rho[row_index], sigma)
+    return hd_prob
+
+
+def ce_gradient(p: np.ndarray, ld_data: np.ndarray, a: float, b: float) -> np.ndarray:
+    """
+    :param p:
+    :param ld_data:
+    :param a:
+    :param b:
+    :return:
+    """
+    ld_data_diff = np.expand_dims(ld_data, 1) - np.expand_dims(ld_data, 0)
+    inv_dist = np.power(1 + a * np.square(euclidean_distances(ld_data, ld_data)) ** b, -1)
+    Q = np.dot(1 - p, np.power(0.001 + np.square(euclidean_distances(ld_data, ld_data)), -1))
+    np.fill_diagonal(Q, 0)
+    Q = Q / np.sum(Q, axis=1, keepdims=True)
+    fact = np.expand_dims(a * p * (1e-8 + np.square(euclidean_distances(ld_data, ld_data))) ** (b - 1) - Q, 2)
+    return 2 * b * np.sum(fact * ld_data_diff * np.expand_dims(inv_dist, 2), axis=1)
+
+
+def ce(p: np.ndarray, ld_data: np.ndarray, a: float, b: float) -> float:
+    """
+    :param p:
+    :param ld_data:
+    :param a:
+    :param b:
+    :return:
+    """
+    Q = np.power(1 + a * np.square(euclidean_distances(ld_data, ld_data))**b, -1)
+    return - p * np.log(Q + 0.01) - (1 - p) * np.log(1 - Q + 0.01)
+
+
+def umap(x: np.ndarray, labels: np.ndarray, min_dist: float,  k_neigh: int,
          no_dims: int = 2, max_iter: int = 1000) -> np.ndarray:
     """
-    Runs t-SNE on the dataset in the NxD array x
+    Runs umap on the dataset in the NxD array x
     to reduce its dimensionality to no_dims dimensions.
     :param x: n x d high dimension data with
+    :param labels: n x 1 the label of high dimensional data
     :param min_dist: float
-    :param beta: the parameter in Gaussian distribution
     :param a: parameter in the family of curves in low dimensional distribution
     :param b: parameter in the family of curves in low dimensional distribution
     :param k_neigh: number of sampling neighbors
@@ -100,87 +174,39 @@ def tsne(x: np.ndarray, min_dist: float, beta: float, a: float, b: float, k_neig
         return -1
 
     # initialize parameters
-
     # x = pca(x, initial_dims).real
-    n = len(x)
-    initial_momentum = 0.5
-    final_momentum = 0.8
-    eta = 500
-    min_gain = 10
-    bound = 1000
-    y = np.random.randn(n, no_dims)
-    # y = np.random.rand(n, 2) * 2 * bound - bound
-    dy = np.zeros((n, no_dims))
-    iy = np.zeros((n, no_dims))
-    gains = np.ones((n, no_dims))
+    n = x.shape[0]
+    learning_rate = 1
 
-    # symmetric
+    # high dimensional distribution
 
-    # P = search_prob(x, 1e-5, perplexity)
-    h_prob = cal_pairwise_dist(x)
-    h_prob = (h_prob + np.transpose(h_prob)) - h_prob * np.transpose(h_prob)
+    h_prob = cal_hd_prob(x, k_neigh)
+    h_prob = (h_prob + np.transpose(h_prob)) / 2
 
-    # early exaggeration
-    h_prob = h_prob * 4
-    h_prob = np.maximum(h_prob, 1e-12)
+    # low dimensional distribution
+    test_x = np.linspace(0, 3, 300)
+    para = scipy.optimize.curve_fit(func, test_x, ld_psai_function(test_x, min_dist))
+    a = para[0][0]
+    b = para[0][1]
+    model = SpectralEmbedding(n_components=no_dims, n_neighbors=50)
+    y = model.fit_transform(np.log(x + 1))
 
     # Run iterations
-
-    for iter in range(max_iter):
-        # Compute pairwise affinities
-        ld_data = y
-        a, b = fitting_curves(ld_psai_function(ld_data, min_dist))
-
-        sum_y = np.sum(np.square(y), 1)
-        # num = 1 / (1 + a * np.power(np.maximum(np.add(np.add(-2 * np.dot(y, y.T), sum_y).T, sum_y), 1e-12), b))
-        num = 1 / (1 + np.add(np.add(-2 * np.dot(y, y.T), sum_y).T, sum_y))
-        num[range(n), range(n)] = 0
-        l_prob = num / np.sum(num)
-        l_prob = np.maximum(l_prob, 1e-12)
-
-        # Compute gradient
-
-        diff_prob = h_prob - l_prob
-        times_prob = h_prob * l_prob
-        diff_times_prob = h_prob - times_prob
-        for i in range(n):
-            # dy[i, :] = 2 * a * b * np.sum(np.tile(diff_times_prob[:, i] * num[:, i] * (fre[i] + fre), (no_dims, 1)).T * (y[i, :] - y), 0)
-
-            # dy[i, :] = 2 * a * b * np.sum(np.tile(diff_prob[:, i] * num[:, i] * (fre[i] + fre), (no_dims, 1)).T * (y[i, :] - y), 0)
-            # test_y = y[i, :] - y
-            # test_y = np.where(test_y == 0, 1e-12, test_y)  # notation needs change
-            # test_y_sub = np.power(test_y+0j, 2 * b-1)
-            # final_y = np.where(test_y == 0, test_y, test_y_sub)
-            # dy[i, :] = 2 * a * b*np.sum(np.tile(diff_prob[:, i] * num[:, i], (no_dims, 1)).T * final_y, 0)
-
-            dy[i, :] = np.sum(np.tile(diff_prob[:, i] * num[:, i], (no_dims, 1)).T * (y[i, :] - y), 0)
-
-        # Perform the update
-        if iter < 20:
-            momentum = initial_momentum
-        else:
-            momentum = final_momentum
-        gains = (gains + 0.2) * ((dy > 0) != (iy > 0)) + (gains * 0.8) * ((dy > 0) == (iy > 0))
-        gains[gains < min_gain] = min_gain
-
-        iy = momentum * iy - eta * (gains * dy)
-        y = y + iy
-        y = y - np.tile(np.mean(y, 0), (n, 1))
-
-        # Compute current value of cost function
-
-        if (iter + 1) % 100 == 0:
-            if iter > 100:
-                loss = np.sum(h_prob * np.log(h_prob / l_prob))
-            else:
-                loss = np.sum(h_prob / 4 * np.log(h_prob / 4 / l_prob))
-            print("Iteration ", (iter + 1), ": error is ", loss)
-
-        # Stop lying about P-values
-
-        if iter == 100:
-            h_prob = h_prob / 4
-    print("finished training!")
+    ce_array = []
+    for i in range(max_iter):
+        y = y - learning_rate * ce_gradient(h_prob, y, a, b)
+        ce_current = np.sum(ce(h_prob, y, a, b)) / 1e+5
+        ce_array.append(ce_current)
+        if i % 10 == 0:
+            plt.figure(figsize=(20, 15))
+            plt.scatter(y[:, 0], y[:, 1], c=labels.astype(int), cmap='tab10', s=50)
+            plt.title("UMAP on Cancer Associated Fibroblasts (CAFs): Programmed from Scratch",
+                      fontsize=20)
+            plt.xlabel("UMAP1", fontsize=20)
+            plt.ylabel("UMAP2", fontsize=20)
+            plt.savefig('figure/UMAP_iter_' + str(i) + '.png')
+            plt.close()
+            print("Cross-Entropy = " + str(ce_current) + " after " + str(i) + " iterations")
     return y
 
 
@@ -190,74 +216,7 @@ if __name__ == "__main__":
     # labels = np.loadtxt("mnist2500_labels.txt")
     data, label1, label2 = sim.simulation_run_1()
     X = data.numpy()
-    Y = tsne(X, label1, 1 / 2, 1.987, 0.98, k_neigh=2, alpha=3.0)
-    fig, ax = plt.subplots()
-    title_str = 'Tsne dimensional reduction for 3motif with random{}' \
-        .format('k_neigh=10')
-    sc = ax.scatter(Y[:, 0], Y[:, 1], c=label2, cmap='tab10')
-    ax.legend(*sc.legend_elements(), title="clusters")
-    plt.show()
-
-    '''
-    alpha_set = np.linspace(1, 2.5, 50)
-    for alpha_i in alpha_set:
-        Y = tsne(X, label1, 1 / 2, 1.987, 0.98, k_neigh = alpha_i)
-        fig, ax = plt.subplots()
-        title_str = 'Tsne dimensional reduction for 3motif with random(weight-loss) alpha={}' \
-            .format(str(k_i))
-        sc = ax.scatter(Y[:, 0], Y[:, 1], c=label2, cmap='tab10')
-        ax.legend(*sc.legend_elements(), title="clusters")
-        plt.title(title_str)
-        plt.savefig("./figure/figure_loss_alpha={}.png".format(str(k_i)))
+    label = label2.numpy()
+    Y = umap(X, label, 0.1, 8)
 
 
-    k = np.linspace(1, 2.5, 50)
-    for k_i in k:
-        Y = tsne(X, label1, 1 / 2, 1, 1, k_i)
-        fig, ax = plt.subplots()
-        title_str = 'Tsne dimensional reduction for 3motif with random(weight-loss) alpha={}' \
-            .format(str(k_i))
-        sc = ax.scatter(Y[:, 0], Y[:, 1], c=label2, cmap='tab10')
-        ax.legend(*sc.legend_elements(), title="clusters")
-        plt.title(title_str)
-        plt.savefig("./figure/figure_loss_alpha={}.png".format(str(k_i)))  
-    k = 10
-    Y = tsne(X, label1, 1 / 2, 1.987, 0.98, k)
-    fig, ax = plt.subplots()
-    sc = ax.scatter(Y[:, 0], Y[:, 1], c=label2, cmap='tab10')
-    ax.legend(*sc.legend_elements(), title="clusters")
-    title_str = 'Tsne dimensional reduction for 3motif with random(weight'
-    plt.title(title_str)
-    plt.show()
-    k = 10
-    a_values = np.linspace(1.1, 2, 10)
-    b_values = np.linspace(0.1, 1, 10)
-    for index_a in a_values:
-        for index_b in b_values:
-            Y = tsne(X, label1, 1/2, 1, 1, k)
-            fig, ax = plt.subplots()
-            title_str = 'Tsne dimensional reduction for 3motif with random(different-t a={},b={}' \
-                .format(str(index_a), str(index_b))
-            sc = ax.scatter(Y[:, 0], Y[:, 1], c=label2, cmap='tab10')
-            ax.legend(*sc.legend_elements(), title="clusters")
-            plt.title(title_str)
-            plt.savefig("./figure/figure_a={},b={}.png".format(str(index_a), str(index_b)))
-
-    #plt.savefig("./figure/figure_sample_k={}.png".format(str(k)))
-    # Y = pca(X, 2).real
-    # fun = MDS(2)
-    # Y = fun.fit_transform(X)
-
-    a_values = np.linspace(1, 2, 5)
-    b_values = np.linspace(0.1, 1, 5)
-    for index_a in a_values:
-        for index_b in b_values:
-            Y = tsne(X, label1, 1/2, index_a, index_b)
-            fig, ax = plt.subplots()
-            title_str = 'Tsne dimensional reduction for 3motif with random(different-t a={},b={}' \
-                .format(str(index_a), str(index_b))
-            sc = ax.scatter(Y[:, 0], Y[:, 1], c=label2, cmap='tab10')
-            ax.legend(*sc.legend_elements(), title="clusters")
-            plt.title(title_str)
-            plt.savefig("./figure/figure_a={},b={}.png".format(str(index_a), str(index_b)))
-    '''
